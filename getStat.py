75568d1a03b8c6b92705e2a38582c133da4034ed
Aiswarya S <aiswaryas@saiswarya-a01.vmware.com>
2020-03-13 18:22:17 +0530

    locked

--- getStat.py
+++ getStat.py
@@ -7,7 +7,7 @@ import subprocess
 import time
 import threading
 from threading import _Timer
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import ast
 import sys
 import random
@@ -21,7 +21,8 @@ files = {
 	"flowcount" : "output/flowcount.txt",
 	"counters" : "output/counters.json",
 	"perf stat" : "output/perf/stat/",
-	"perf report" : "output/perf/report/"
+	"perfReport_cpu" : "output/perf/report/cpu/",
+	"perfReport_drops" : "output/perf/report/drops/"
 }
 
 commandList = {
@@ -76,18 +77,25 @@ def executeCommand(command) :
 	result = commands.getoutput(command)
 	return result
 
-def getThread(thread) :
+def getCpuPercent(proc, t):
+   total_percent = proc.cpu_percent(interval = 0.1)
+   total_time = sum(proc.cpu_times())
+   return total_percent * ((t.system_time + t.user_time)/total_time)
+
+def getThread(proc, thread) :
 	try:
 		threadEntry = {}
 		threadEntry['tid'] = thread.id
 		t = psutil.Process(thread.id)
 		threadEntry['name'] = t.name()
-		threadEntry['cpu_percent'] = t.cpu_percent(interval = 0.01) 
+		threadEntry['cpu_percent'] = getCpuPercent(proc, thread)
 		threadEntry['user_time'] = thread.user_time
 		threadEntry['system_time'] = thread.system_time
 
+		print("{0} {1} {2}".format(threadEntry['tid'], threadEntry['name'], threadEntry['cpu_percent']))
+
 		return threadEntry
-	except :
+	except:
 		pass
 
 def getProcessSamples(proc) :
@@ -98,29 +106,53 @@ def getProcessSamples(proc) :
 	sample[timeStamp]['threads'] = []
 
 	for thread in proc.threads() :
-		sample[timeStamp]['threads'].append(getThread(thread))
-
+		t = CustomTimer(0, getThread, [proc, thread])
+		t.start()
+		sample[timeStamp]['threads'].append(t.join())
 	return sample
 
 def findIndex(data, proc) :
 	for index in range(len(data['all'])) :
 		if proc.pid == data['all'][index]['pid'] :
 			return index
+	return -1
 
-def collectNextProcessSample(input) :
+def collectNextProcessSample(input, lock) :
 	for process in input["cpu"]:
 		fileAddr = getFileAddr(process)
-		data = loadData(fileAddr)
+		with lock:
+			data = loadData(fileAddr)
 
-		if process == 'all' :
-			for proc in psutil.process_iter():
-				index = findIndex(data, proc)
-				data[process][index]['samples'].append(getProcessSamples(proc))
-		else :
-			for proc in psutil.process_iter():
-				if proc.name() == process:
-					data[process]['samples'].append(getProcessSamples(proc))
-		writeFile(data, fileAddr)
+			if process == 'all' :
+				for proc in psutil.process_iter():
+					index = findIndex(data, proc)
+					if(index != -1):
+						t = CustomTimer(0, getProcessSamples, [proc])
+						t.start()
+						print("\t\t {0}".format(len(data[process][index]['samples'])))
+						data[process][index]['samples'].append(t.join())
+					else :
+						newProcess = getProcessDetails(proc)
+						t = CustomTimer(0, getProcessSamples, [proc])
+						t.start()
+						print("\t\t {0}".format(len(data[process]['samples'])))
+						newProcess['samples'].append(t.join())
+						data[process].append(newProcess)
+			else :
+				try:
+					pid = data[process]["pid"]
+					t = CustomTimer(0, getProcessSamples, [psutil.Process(pid)])
+					t.start()
+					print("\t\t {0}".format(len(data[process]['samples'])))
+					data[process]['samples'].append(t.join())
+				except:
+					pass
+				# for proc in psutil.process_iter():
+				# 	if proc.name() == process:
+				# 		data[process]['samples'].append(getProcessSamples(proc))
+			writeFile(data, fileAddr)
+
+
 
 def getProcessDetails(proc) :
 
@@ -132,6 +164,9 @@ def getProcessDetails(proc) :
 	return processEntry
 
 def getCpuProfile(input, noOfSample, sampleFrequency):
+
+	lock = threading.Lock()
+
 	for process in input["cpu"]:
 		output = {}
 		if process == 'all' :
@@ -141,9 +176,12 @@ def getCpuProfile(input, noOfSample, sampleFrequency):
 		else :
 			output[process] = {}
 			for proc in psutil.process_iter():
-				if proc.name() == process:
-					output[process] = getProcessDetails(proc)
-					break
+				try:
+					if proc.name() == process:
+						output[process] = getProcessDetails(proc)
+						break
+				except:
+					print("process stopped unexpectedly")
 
 		fileAddr = getFileAddr(process)
 		writeFile(output, fileAddr)
@@ -153,44 +191,13 @@ def getCpuProfile(input, noOfSample, sampleFrequency):
 	delay = 0
 	while noOfSample > 0 :
 		noOfSample -= 1
-		tObj = threading.Timer(delay, collectNextProcessSample, [input])
+		tObj = threading.Timer(delay, collectNextProcessSample, [input, lock])
 		tObj.start()
 		t.append(tObj)
 		delay += sampleFrequency
 
 	for tObj in t:
 		tObj.join()
-
-def findCriticalThreads() :
-
-	handoff = loadData(files["handoff"]) 
-	handoff = poisonQueue(handoff)
-
-	rateOfIncrease = {}
-
-	nTS = len(handoff["handoff"]) - 1
-	fTS = handoff["handoff"][0].items()[0][0]
-	lTS = handoff["handoff"][nTS].items()[0][0]
-
-	for queue in range(len(handoff["handoff"][0][fTS]["handoffq"])):
-		rateOfIncrease[handoff["handoff"][0][fTS]["handoffq"][queue]["tid"]] = handoff["handoff"][nTS][lTS]["handoffq"][queue]["drops"] - handoff["handoff"][0][fTS]["handoffq"][queue]["drops"]
-	
-	# sort
-	sortedRes  = OrderedDict()
-	for key, value in sorted(rateOfIncrease.items(), key = lambda item : item[1], reverse = True):
-		sortedRes[key] = value
-
-	# extract top 10
-	top10 = OrderedDict()
-	count = 0
-	for key in sortedRes.keys():
-		if count < 10 :
-			top10[key] = sortedRes[key]
-			count +=1
-		else :
-			break
-
-	return top10
 
 def modifyDrop(drop) :
 	drop += random.randint(1, 1000)
@@ -209,6 +216,66 @@ def poisonQueue(handoff) :
 			ts[ts.items()[0][0]]["handoffq"][queue]["drops"] = modifyDrop(pre[pre.items()[0][0]]["handoffq"][queue]["drops"])
 		pre = ts
 	return handoff
+
+def findCriticalThreads_dropBased() :
+
+	handoff = loadData(files["handoff"]) 
+	handoff = poisonQueue(handoff)
+
+	threadWithDrop = {}
+
+	nTS = len(handoff["handoff"]) - 1
+	fTS = handoff["handoff"][0].items()[0][0]
+	lTS = handoff["handoff"][nTS].items()[0][0]
+
+	for queue in range(len(handoff["handoff"][0][fTS]["handoffq"])):
+		threadWithDrop[handoff["handoff"][0][fTS]["handoffq"][queue]["tid"]] = handoff["handoff"][nTS][lTS]["handoffq"][queue]["drops"] - handoff["handoff"][0][fTS]["handoffq"][queue]["drops"]
+
+	return threadWithDrop
+
+def findCriticalThreads_cpuBased() :
+
+	data = loadData(getFileAddr("all"))
+	threadWithCpuPercent = defaultdict(list)
+
+	for process in data["all"] :
+		for ts in process["samples"] :
+			for thread in ts[ts.items()[0][0]]["threads"] :
+				try:
+					threadWithCpuPercent[thread["tid"]].append(thread["cpu_percent"])
+				except:
+					pass
+
+	for key in threadWithCpuPercent :
+		print(str(key) + " - " + str(threadWithCpuPercent[key]))
+		# print(str(key) + " - " + psutil.Process(key).name() + " - " + str(threadWithCpuPercent[key]))
+		threadWithCpuPercent[key] = sum(threadWithCpuPercent[key]) / len(threadWithCpuPercent[key])
+
+	return threadWithCpuPercent
+
+def findCriticalThreads(key) :
+
+	if key == "cpu" :
+		resDict = findCriticalThreads_cpuBased()
+	elif key == "drops" :
+		resDict = findCriticalThreads_dropBased()
+
+	sortedRes = OrderedDict()
+
+	for key, value in sorted(resDict.items(), key = lambda item : item[1], reverse = True):
+		sortedRes[key] = value
+
+	top10 = OrderedDict()
+	count = 0
+	for key in sortedRes.keys():
+		if count < 10 :
+			top10[key] = sortedRes[key]
+			count +=1
+		else :
+			break
+
+	print(top10)
+	return top10
 
 def getHandoff(fileAddr, noOfSample, sampleFrequency) :
 	output = {}
@@ -343,15 +410,17 @@ def perfRecord(duration) :
 def isRecordDone():				# for now
 	return True
 
-def perfReport(outputDirectory, criticalThreads) :
+def perfReport(outputDirectory, criticalThreads, key) :
 	while isRecordDone() == False:
 		continue
+
 	top10Threads = [item[0] for item in criticalThreads.items()]
 
 	createDirectory(outputDirectory + "/perf/report")
+	createDirectory(outputDirectory + "/perf/report/" + key)
 
 	for tid in top10Threads:
-		fileName = generateFileName(getFileAddr("perf report"), tid, 1)
+		fileName = generateFileName(getFileAddr("perfReport_{0}".format(key)), tid, 1)
 		res = executeCommand("perf report --call-graph=none --tid={0} > {1}".format(tid, fileName))
 		
 
@@ -369,12 +438,13 @@ def main():
  	collectStat(input, noOfSample)
 
  	# find critical threads
-	criticalThreads = findCriticalThreads()
-	print(criticalThreads)
+	criticalThreads = findCriticalThreads("drops")
+	perfReport(input['outputDirectory'], criticalThreads, "drops")
 
-	perfReport(input['outputDirectory'], criticalThreads)
+	criticalThreads = findCriticalThreads("cpu")
+	perfReport(input['outputDirectory'], criticalThreads, "cpu")
 
-	zipOutput()
+	# zipOutput()
 
 
 
